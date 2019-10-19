d0a893bd226a0f860675e9764bed91c4e524632a
Felix Stephen <felsen88@gmail.com>
2019-10-19 14:44:13 +0530

    Initial commit: get all the hash tags based on search keyword

---
+++ tweetags/twitterapi.py
@@ -0,0 +1,64 @@
+import tweepy
+import re
+
+
+API_KEY = ""
+API_SECRET_KEY = ""
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_KEY = ""
+
+
+def tweepy_auth(api_key, api_secret_key, access_token, access_token_key):
+    """Authentication using twitter api_key, api_secret_key, access_token,
+    and access_token_key
+
+    api_key: get the key from twitter developer account.
+    api_secret_key: get the secret key from twitter developer account.
+    access_token: get the access token from twitter developer account.
+    access_token_secret: get the access token key from twitter developer account.
+    returns: it returns twitter authentication api.
+    """
+    auth = tweepy.OAuthHandler(api_key, api_secret_key)
+    auth.set_access_token(access_token, access_token_key)
+    api = tweepy.API(auth)
+    return api
+
+
+def keyword_search(keyword):
+    """Keyword search function to returns related hash tags.
+
+    keyword: input text to search all the twitter hash tags.
+    returns: list of unique hash tags realated to input keyword.
+    """
+    api_key, api_secret_key = API_KEY, API_SECRET_KEY
+    access_token, access_token_key = ACCESS_TOKEN, ACCESS_TOKEN_KEY
+    api = tweepy_auth(api_key, api_secret_key, access_token, access_token_key)
+    hash_tag = []
+    for text in api.search(q=keyword, lang="en", ):
+        tags = re.findall(r"(#\w+)", str(text))
+        hash_tag.extend(tags)
+    return set(hash_tag)
+
+
+def hasgtag_followers(tags):
+    """Get the number of followers, based on the hash tags.
+    
+    tags: list of hash tags is input to get number of followers.
+    returns:
+    """
+    
+    return tags 
+
+
+def hashtag_recent_tweets():
+    """Get the recent tweets, based on hash tags.
+
+    """
+    return None
+
+
+
+if __name__ == '__main__':
+    tags = keyword_search("Python")
+    followers = hasgtag_followers(tags)
+    print(followers)
